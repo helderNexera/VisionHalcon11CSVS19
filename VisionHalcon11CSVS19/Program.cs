21ce427f87a1c00e76ece9b2732c70809d1e0eab
Helder <helder.fonseca@nexera.ch>
2021-11-02 11:43:39 +0100

    Add project files.

---
+++ VisionHalcon11CSVS19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HalconDotNet;
+
+namespace VisionHalcon11CSVS19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !!!");
+            HOperatorSet.SetSystem("width", 512);
+        }
+    }
+}
